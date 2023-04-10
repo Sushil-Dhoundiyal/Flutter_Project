3bdf95de8e8ed308b8bc0cb1f1bc55bd16226a28
Sushil Dhoundiyal <sushil181282@gmail.com>
2023-04-10 14:18:46 +0530

    day2 completed

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,21 +1,17 @@
 
 import 'package:flutter/material.dart';
 
-import 'screens/home_screen.dart';
+import 'home_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home:Homescreen(),
+      home: HomePage(),
     );
   }
 }
