d9727a1bc74d61d8b6bf339ea26c7aacf269f886
Sushil Dhoundiyal <sushil181282@gmail.com>
2023-04-11 17:23:24 +0530

    day4 completed

--- firstapp/lib/pages/login_page.dart
+++ firstapp/lib/pages/login_page.dart
@@ -6,14 +6,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child:Text("Login page", 
-        style: TextStyle(
-          fontSize: 20, color: Colors.yellow, fontWeight: FontWeight.bold
-        ),
-        textScaleFactor: 2.0,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png", fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+            // child: Text('hello'),
+          ),
+          Text('Welcome',
+          style: TextStyle(
+            fontSize: 23, fontWeight: FontWeight.bold
+            ),
+            ),
+            SizedBox(
+            height: 20.0,
+            // child: Text('hello'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter username",
+                labelText: "Username"
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter password",
+                labelText: "Password"
+              ),
+            ),
+            SizedBox(
+            height: 20.0,
+            // child: Text('hello'),
+          ),
+          ElevatedButton(
+            child: Text('Login'),
+            style: TextButton.styleFrom(),
+            onPressed: () {
+              print('hi sushil')
+            },
+          )
+              ],
+            ),
+          )
+        ],
         )
-         ),
     );
   }
 }
