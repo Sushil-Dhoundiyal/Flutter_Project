6f843fe8e8ce382af2786061c1132cbf4018454d
Sushil Dhoundiyal <sushil181282@gmail.com>
2023-04-18 18:39:46 +0530

    themes extracted

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firstapp/pages/login_page.dart';
 import 'package:firstapp/utils/routes.dart';
+import 'package:firstapp/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -12,16 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
--- firstapp/lib/pages/home_page.dart
+++ firstapp/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firstapp/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
