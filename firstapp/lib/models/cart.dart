58acf1ba5fa5784d1a6586ff2549f0294a9b789f
Sushil Dhoundiyal <sushil181282@gmail.com>
2023-04-21 00:59:26 +0530

    day 27 completed

--- firstapp/lib/models/cart.dart
+++ firstapp/lib/models/cart.dart
@@ -24,11 +24,6 @@ List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
 num get totalPrice => items.fold(0, (total, current) => total + current.price);
 
 
-// REMOVE ITEM
-void remove(Item item){
-  _itemIds.remove(item.id);
-}
-
 
 }
 
@@ -40,6 +35,18 @@ class AddMutation extends VxMutation<MyStore>{
   @override
   perform() {
     store!.cart._itemIds.add(item.id);        //   !  **************************
+  }
+  
+}
+
+
+class RemoveMutation extends VxMutation<MyStore>{
+  final Item item;
+
+  RemoveMutation(this.item);
+  @override
+  perform() {
+    store!.cart._itemIds.remove(item.id);        //   !  **************************
   }
   
 }
